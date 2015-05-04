bd4e60ce55d638e1d0562cbbb7b643a8ab1a7f7a
Plamen Ivanov <dramatron@gmail.com>
2015-05-04 13:01:11 +0300

    Second
    Commit

--- WebApiDemo/Controllers/ValuesController.cs
+++ WebApiDemo/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace WebApiDemo.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "demo" };
         }
 
         // GET api/values/5
